a3e02a046aa690f8ddc97fdfcbe03e36b38c73f6
Tharunkumar G <tharung1411@gmail.com>
2025-12-28 18:32:07 +0530

    endpoint update

--- homeAndCheckout/src/utils/endpoint.js
+++ homeAndCheckout/src/utils/endpoint.js
@@ -1,7 +1,8 @@
-const BASEURL = "https://kicks-backend-seven.vercel.app/"
+const BASEURL = "https://kicksms.vercel.app/";
 // const BASEURL = "http://localhost:4000/"
 
 export const GET_HOME_DETAILS = BASEURL + `getHomeDetails`;
-export const GET_PRODUCT_DETAILS = (id) => BASEURL + `getProductDetails?productId=${id}`;
-export const GET_PRODUCT_LIST  = BASEURL + `getProductList`;
-export const GET_FILTER_PROPERTIES  = BASEURL + `getFilterProperties`;
+export const GET_PRODUCT_DETAILS = (id) =>
+  BASEURL + `getProductDetails?productId=${id}`;
+export const GET_PRODUCT_LIST = BASEURL + `getProductList`;
+export const GET_FILTER_PROPERTIES = BASEURL + `getFilterProperties`;
